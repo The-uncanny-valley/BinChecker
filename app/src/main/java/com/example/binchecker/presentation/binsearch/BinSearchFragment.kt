ac2aad4fe19566a3b377d57a99297a5961287f56
the-uncanny-valley <runcannyvalley@gmail.com>
2025-08-06 16:37:05 +0300

    refactor: Convert TextView to clickable element for navigating to BinHistoryFragment - Assigned ID: buttonHistory - Set onClickListener in BinSearchFragment.kt

--- app/src/main/java/com/example/binchecker/presentation/binsearch/BinSearchFragment.kt
+++ app/src/main/java/com/example/binchecker/presentation/binsearch/BinSearchFragment.kt
@@ -1,9 +1,10 @@
 package com.example.binchecker.presentation.binsearch
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.View
+import android.widget.TextView
 import android.widget.Toast
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.binchecker.R
 import com.example.binchecker.data.RetrofitInstance
@@ -21,6 +22,7 @@ class BinSearchFragment : Fragment(R.layout.fragment_bin_search) {
 
         val buttonSearch = view.findViewById<MaterialButton>(R.id.buttonSearch)
         val editBin = view.findViewById<TextInputEditText>(R.id.editBin)
+        val buttonHistory = view.findViewById<TextView>(R.id.buttonHistory)
 
         buttonSearch.setOnClickListener {
             val inputText = editBin.text.toString().trim()
@@ -28,10 +30,14 @@ class BinSearchFragment : Fragment(R.layout.fragment_bin_search) {
                 Toast.makeText(requireContext(), "This field is empty", Toast.LENGTH_LONG).show()
             } else if (inputText.length == 6 || inputText.length == 8) {
                 loadBinInfo(inputText)
-
             } else {
                 Toast.makeText(requireContext(), "Please enter 6 or 8 digits", Toast.LENGTH_SHORT).show()
             }
+        }
+
+        buttonHistory.setOnClickListener {
+            val action = BinSearchFragmentDirections.actionBinSearchFragmentToBinHistoryFragment()
+            findNavController().navigate(action)
         }
     }
 
